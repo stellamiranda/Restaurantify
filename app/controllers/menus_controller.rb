12966301f45619bff626528d443eedb4e9ec5a2e
oscar <oscar@oscar-desktop.(none)>
2009-07-09 17:35:03 -0500

    merge with branch master oscar pc

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -29,7 +29,9 @@ class MenusController < ApplicationController
 
     def show
      @menus = Menu.find(params[:id])
-
+     @menucategories = @menus.menucategories
+   #  @category = @menucategories.category.id
+     @restaurant = Restaurant.find(params[:restaurant_id])
       respond_to do |format|
       format.html # show.html.erb
       format.xml  { render :xml =>  @menus}
